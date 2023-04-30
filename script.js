120e3dcca517b11114b4ddd80b1da9fcbbe6d8b8
Thomas Chia <tchia97@gmail.com>
2023-04-30 17:10:55 +0100

    Add clear button.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Get references to DOM elements
 const fileInput = document.getElementById('file-input');
 const classifyButton = document.getElementById('classify-button');
+const clearButton = document.getElementById('clear-button');
 const resultContainer = document.getElementById('result-container');
 const image = document.getElementById('uploaded-image');
 
-// Add event listener for file input
+// Add event listeners for file input, classify button, and clear button
 fileInput.addEventListener('change', handleUpload);
-
-// Add event listener for classify button
 classifyButton.addEventListener('click', handleClassify);
+clearButton.addEventListener('click', handleClear);
 
 // Handle file upload
 function handleUpload(event) {
@@ -20,4 +20,10 @@ function handleUpload(event) {
 function handleClassify() {
   // Replace this with your API call to classify the bird image
   resultContainer.innerText = 'Classification result';
+}
+
+// Handle clear button click
+function handleClear() {
+  image.src = '';
+  fileInput.value = '';
 }
